1be9268521289ccc9fad48dbeda58d8bb48105e3
LAMSSAOUI ABDELALI <lam998216@gmail.com>
2022-10-01 19:35:44 +0200

    this is the first try of material ui

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import { Typography } from '@mui/material';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+    <Navbar />
+    <Typography variant='h1' align='center'>
+        Bonjour 
+    </Typography>
+    </>
+    
+  )
+}
+
+
+export default App
